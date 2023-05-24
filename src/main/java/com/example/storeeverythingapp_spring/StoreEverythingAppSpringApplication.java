b3b89420bdbfc932cf559f77c935402e61dd56f7
Sonia Becz <sonia.becz@onet.pl>
2023-05-24 11:53:14 +0200

    Initial commit

---
+++ src/main/java/com/example/storeeverythingapp_spring/StoreEverythingAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.storeeverythingapp_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreEverythingAppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreEverythingAppSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/storeeverythingapp_spring/StoreEverythingAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storeeverythingapp_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreEverythingAppSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
