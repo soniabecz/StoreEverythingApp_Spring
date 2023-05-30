1f1da5d080faed157848df0ef0a78bf8c3178f38
Sonia Becz <sonia.becz@onet.pl>
2023-05-30 21:28:21 +0200

    category validation

--- src/main/java/com/example/storeeverythingapp_spring/data/Information.java
+++ src/main/java/com/example/storeeverythingapp_spring/data/Information.java
@@ -10,6 +10,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.validation.annotation.Validated;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
