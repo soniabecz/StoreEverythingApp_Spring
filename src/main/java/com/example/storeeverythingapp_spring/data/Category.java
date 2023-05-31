797d16ce08f73c3f6b0d8e6fbb602af931e68d03
Sonia Becz <sonia.becz@onet.pl>
2023-05-31 09:25:24 +0200

    repairing category validation

--- src/main/java/com/example/storeeverythingapp_spring/data/Category.java
+++ src/main/java/com/example/storeeverythingapp_spring/data/Category.java
@@ -13,8 +13,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Category {
 
-    @Size(min = 3, max = 20, message = "Name has to consist of more than 3 and less than 20 characters")
-    @Pattern(regexp = "[a-z]+", message = "Name should only consist of lowercase letters")
+    @Size(min = 3, max = 20, message = "Name has to consist of more than 3 and less than 20 characters", groups = {CategoryValidation.class})
+    @Pattern(regexp = "[a-z]+", message = "Name should only consist of lowercase letters", groups = {CategoryValidation.class})
     @NotBlank(message = "Category has to be chosen")
     @CategoryValidation(groups = {CategoryValidation.class})
     private String name;
